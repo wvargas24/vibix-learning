75d15f3b3fdc386a945eb3855c6257553e6065ca
Wuilly Vargas <wuilly.vargas@interkont.co>
2022-07-29 11:28:27 -0500

    Set colors and form of modal Suppor Student

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,4 @@
-$(document).ready(() => {
-    $("#edit-profile-button").on( "click", function() {
-        console.log( $( this ).text() );
-        $( this ).css('display','none');
-        var text = $("#about-me-text");
-        var interests = $("#about-me-interests");
-        var paragraph = $(".navbar-dropdown-account p");
-        var button = $("#save-profile-button");
 
-        text.css('display','inline-block');
-        interests.css('display','inline-block');
-        button.css('display','inline-block');
-        paragraph.css('display','none');
-    });
-    
-    $("#save-profile-button").on( "click", function() {
-        console.log( $( this ).text() );
-        $( this ).css('display','none');
-        var text = $("#about-me-text");
-        var interests = $("#about-me-interests");
-        var paragraph = $(".navbar-dropdown-account p");
-        var button = $("#edit-profile-button");
-
-        text.css('display','none');
-        interests.css('display','none');
-        button.css('display','inline-block');
-        paragraph.css('display','inline-block');
-    });
-    
-    $(".ts-control>input").on({
-        change: function (e) {
-            e.preventDefault();
-        },
-        keyup: function () {
-            $( this ).siblings().remove();
-        },
-        blur: function () {
-            $( this ).siblings().remove();
-        }
-    });
-
-});
 
 
 (function() {
@@ -48,4 +7,92 @@ $(document).ready(() => {
       // =======================================================
       HSCore.components.HSTomSelect.init('.js-select')
     }
+
+    const validateButton = (element, button) =>{
+        if ( element.val() != '') {
+            $(button).prop("disabled", false);
+        }else{
+            $(button).prop("disabled", true);
+        }
+    }
+
+    $(document).ready(() => {
+        $("#edit-profile-button").on( "click", function() {
+            console.log( $( this ).text() );
+            $( this ).css('display','none');
+            var text = $("#about-me-text");
+            var interests = $("#about-me-interests");
+            var paragraph = $(".navbar-dropdown-account p");
+            var button = $("#save-profile-button");
+    
+            text.css('display','inline-block');
+            interests.css('display','inline-block');
+            button.css('display','inline-block');
+            paragraph.css('display','none');
+        });
+        
+        $("#save-profile-button").on( "click", function() {
+            console.log( $( this ).text() );
+            $( this ).css('display','none');
+            var text = $("#about-me-text");
+            var interests = $("#about-me-interests");
+            var paragraph = $(".navbar-dropdown-account p");
+            var button = $("#edit-profile-button");
+    
+            text.css('display','none');
+            interests.css('display','none');
+            button.css('display','inline-block');
+            paragraph.css('display','inline-block');
+        });
+        
+        $(".ts-control input").on({
+            change: function (e) {
+                e.preventDefault();
+            },
+            keyup: function () {
+                validateButton($(this),'#support-student-button');
+            },
+            blur: function () {
+                validateButton($(this),'#support-student-button');            
+            }
+        });
+
+        $("#support-student-date").on({
+            change: function (e) {
+                e.preventDefault();
+            },
+            keyup: function () {
+                validateButton($(this),'#support-student-button');
+            },
+            blur: function () {
+                validateButton($(this),'#support-student-button');            
+            }
+        });
+
+        $("#support-student-time").on({
+            change: function (e) {
+                e.preventDefault();
+            },
+            keyup: function () {
+                validateButton($(this),'#support-student-button');
+            },
+            blur: function () {
+                validateButton($(this),'#support-student-button');            
+            }
+        });
+        
+        $("#support-student-observations").on({
+            change: function (e) {
+                e.preventDefault();
+            },
+            keyup: function () {
+                validateButton($(this),'#support-student-button');
+            },
+            blur: function () {
+                validateButton($(this),'#support-student-button');            
+            }
+        });
+    
+    });
+
 })()
