90c7e97854589778e340ed4a0a2db5cbdffb97d3
Wuilly Vargas <wuilly.vargas@interkont.co>
2022-08-06 19:09:04 -0500

    Create microphone recorder functionality and visual development

--- assets/js/mic.js
+++ assets/js/mic.js
@@ -1,166 +1,341 @@
-(async () => {
-    let volumeCallback = null;
-    let volumeInterval = null;
-    const volumeVisualizer = document.querySelector('#volume-visualizer');
-    const recordBox = document.querySelector('.recording-box');
-    const recordButton = document.querySelector('#record');
-    const stopButton = document.querySelector('#stop');
-    const playButton = document.querySelector('#play');
-    const saveButton = document.querySelector('#save');
-    const savedAudioMessagesContainer = document.querySelector('#saved-audio-messages');
-    const textRecord = document.querySelector("#texActivity");
-    const timeSelector = document.querySelector("#time");
+"use strict";
 
-    let recorder;
-    let audio;
+/**
+ * @author: Sam Bellen
+ * @description: Draw a waveform from the microphone's audio.
+ *               Tested in Chrome and Firefox.
+ */
+(function () {
+    if (!window.AudioContext) {
+        setMessage('Your browser does not support window.Audiocontext. This is needed for this demo to work. Please try again in a differen browser.');
+    } // UI Elements
 
-    const recordAudio = () =>
-        new Promise(async resolve => {
-            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-            const audioContext = new AudioContext();
-            const audioSource = audioContext.createMediaStreamSource(stream);
-            const analyser = audioContext.createAnalyser();
-            analyser.fftSize = 512;
-            analyser.minDecibels = -127;
-            analyser.maxDecibels = 0;
-            analyser.smoothingTimeConstant = 0.4;
-            audioSource.connect(analyser);
-            const bufferLength = analyser.frequencyBinCount;
-            const volumes = new Uint8Array(analyser.frequencyBinCount);
-            volumeCallback = () => {
-                analyser.getByteTimeDomainData(volumes);
-                analyser.getByteFrequencyData(volumes);
-                let volumeSum = 0;
-                for (const volume of volumes)
-                    volumeSum += volume;
-                const averageVolume = volumeSum / volumes.length;
-                volumeVisualizer.style.setProperty('--volume', (averageVolume * 100 / 127) + '%');
-            };
+    const messageContainer = document.querySelector('.js-message');
+    const recorderBox = document.querySelector('.recording-box');
+    const elapsedTimeTag = document.querySelector('.js-time');
+    const removeRecorder = document.querySelector('#removeVoiceNoteConfirm');
+    const canvas = document.querySelector('.js-canvas');
+    const recordButton = document.querySelector('.js-record');
+    const stopButton = document.querySelector('.js-stop');
+    const playButton = document.querySelector('.js-play');
+    const audioPlayer = document.querySelector('.js-audio');
+    const playButtonIcon = document.querySelector('.js-play .bi'); // Constants
 
-            const mediaRecorder = new MediaRecorder(stream);
-            let audioChunks = [];
+    const audioContext = new AudioContext();
+    const analyser = audioContext.createAnalyser();
+    const scriptProcessor = audioContext.createScriptProcessor(2048, 1, 1);
+    const chunks = []; // Variables
 
-            mediaRecorder.addEventListener('dataavailable', event => {
-                audioChunks.push(event.data);
+    let stream = null;
+    let input = null;
+    let recorder = null;
+    let recording = null;
+    let isRecording = false;
+    let isPlaying = false; // Setup analyser node
+
+    analyser.smoothingTimeConstant = 0.3;
+    analyser.fftSize = 1024; // Canvas variables
+
+    const barWidth = 3;
+    const barGutter = 3;
+    const barColor = "#AEAEAE";
+    let canvasContext = canvas.getContext('2d');
+    let bars = [];
+    let width = 0;
+    let height = 0;
+    let halfHeight = 0;
+    let drawing = false; // Show a message in the UI
+
+    var audioRecordStartTime;
+    var elapsedTimeTimer;
+
+    const setMessage = message => {
+        messageContainer.innerHTML = message;
+        messageContainer.classList.add('message--visible');
+    }; // Hide the message
+
+
+    const hideMessage = () => {
+        messageContainer.classList.remove('message--visible');
+    }; // Request access to the user's microphone.
+
+
+    const requestMicrophoneAccess = () => {
+        if (navigator.mediaDevices) {
+            navigator.mediaDevices.getUserMedia({
+                audio: true
+            }).then(stream => {
+                setAudioStream(stream);
+            }, error => {
+                setMessage('Something went wrong requesting the userMedia. <br/>Please make sure you are viewing this demo over https.');
             });
-
-            const start = () => {
-                audioChunks = [];
-                mediaRecorder.start();
-            };
-
-            const stop = () => new Promise(resolve => {
-                mediaRecorder.addEventListener('stop', () => {
-                    const audioBlob = new Blob(audioChunks, { type: 'audio/mpeg' });
-                    const audioUrl = URL.createObjectURL(audioBlob);
-                    const audio = new Audio(audioUrl);
-                    const play = () => audio.play();
-                    resolve({ audioChunks, audioBlob, audioUrl, play });
-                });
-
-                mediaRecorder.stop();
-            });
-
-            resolve({ start, stop });
-    });
-
-    const sleep = time => new Promise(resolve => setTimeout(resolve, time));
-
-    recordButton.addEventListener('click', async () => {
-        recordButton.setAttribute('disabled', true);
-        recordButton.style.display = "none";
-        stopButton.removeAttribute('disabled');
-        stopButton.style.display = "block";
-        recordBox.style.display = "flex";
-        playButton.setAttribute('disabled', true);
-        playButton.style.display = "none";
-        saveButton.setAttribute('disabled', true);
-        if (!recorder) {
-            recorder = await recordAudio();
+        } else {
+        setMessage('Your browser does not support navigator.mediadevices. <br/>This is needed for this demo to work. Please try again in a differen browser.');
         }
+    }; // Set all variables which needed the audio stream
+
+
+    const setAudioStream = stream => {
+        stream = stream;
+        input = audioContext.createMediaStreamSource(stream);
+        recorder = new window.MediaRecorder(stream);
+        setRecorderActions();
+        setupWaveform();
+    }; // Setup the recorder actions
+
+
+    const setRecorderActions = () => {
+        recorder.ondataavailable = saveChunkToRecording;
+        recorder.onstop = saveRecording;
+    }; // Save chunks of the incomming audio to the chuncks array
+
+    const destroyListeners = () => {
+        recorder.ondataavailable = undefined;
+        recorder.onstop = undefined;
+      };
+
+
+    const saveChunkToRecording = event => {
+        chunks.push(event.data);
+    }; // Save the recording
+
+
+    const saveRecording = () => {
+        recording = URL.createObjectURL(new Blob(chunks, {
+        'type': 'audio/ogg; codecs=opus'
+        }));
+
+        chunks: [];
+
+        audioPlayer.setAttribute('src', recording);
+        playButton.classList.remove('button--disabled');
+    }; // Start recording
+
+
+    const startRecording = () => {
+        isRecording = true;
         recorder.start();
-        if (volumeCallback !== null && volumeInterval === null){
-            volumeInterval = setInterval(volumeCallback, 100);
-            textRecord.innerHTML = "Recording...";
+        //store the recording start time to display the elapsed time according to it
+        audioRecordStartTime = new Date();
+    }; // Stop recording
+
+
+    const stopRecording = () => {
+        isRecording = false;
+        recorder.stop();
+    }; // Toggle the recording button
+
+
+    const toggleRecording = () => {
+        if (isRecording) {
+            stopRecording();
+        } else {
+            startRecording();
         }
-    });
+    }; // Setup the canvas to draw the waveform
 
-    stopButton.addEventListener('click', async () => {
-        recordButton.removeAttribute('disabled');
-        stopButton.setAttribute('disabled', true);
-        stopButton.style.display = "none";
-        playButton.removeAttribute('disabled');
-        playButton.style.display = "block";
-        saveButton.removeAttribute('disabled');
-        audio = await recorder.stop();
-        if (volumeInterval !== null) {
-            clearInterval(volumeInterval);
-            volumeInterval = null;
-            var _button = $("<a/>")
-                .addClass("btn btn-info btn-remove-record")
-                .attr("href", "#")
-                .attr("role", "button")
-                .attr("data-toggle", "modal")                
-                .attr("id", "btn_delete_record")
-                .html('<i class="bi bi-trash-fill"></i>')
-                .on("click", function (e) {
-                    e.preventDefault();
-                    var _button = $(this);
-                    //here is call function to delete record
-                    _button.css('display','none');
-                    recordBox.style.display = "none";                    
-                    recordButton.removeAttribute('disabled');
-                    recordButton.style.display = "block";
-                    
-                });
-            $('#texActivity').html(_button);
+
+    const setupWaveform = () => {
+        canvasContext = canvas.getContext('2d');
+        width = canvas.offsetWidth;
+        height = canvas.offsetHeight;
+        halfHeight = canvas.offsetHeight / 2;
+        canvasContext.canvas.width = width;
+        canvasContext.canvas.height = height;
+        input.connect(analyser);
+        analyser.connect(scriptProcessor);
+        scriptProcessor.connect(audioContext.destination);
+        scriptProcessor.onaudioprocess = processInput;
+    }; // Process the microphone input
+
+
+    const processInput = audioProcessingEvent => {
+        if (isRecording) {
+        const array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+        bars.push(getAverageVolume(array));
+
+        if (bars.length <= Math.floor(width / (barWidth + barGutter))) {
+            renderBars(bars);
+        } else {
+            renderBars(bars.slice(bars.length - Math.floor(width / (barWidth + barGutter))), bars.length);
         }
-    });
+        } else {
+        bars = [];
+        }
+    }; // Calculate the average volume
 
-    playButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        audio.play();
-    });
 
-    saveButton.addEventListener('click', () => {
-        const reader = new FileReader();
-        reader.readAsDataURL(audio.audioBlob);
-        reader.onload = () => {
-            const base64AudioMessage = reader.result.split(',')[1];
+    const getAverageVolume = array => {
+        const length = array.length;
+        let values = 0;
+        let i = 0;
 
-            fetch('/messages', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ message: base64AudioMessage })
-            }).then(res => {
-                if (res.status === 201) {
-                    return populateAudioMessages();
-                }
-                console.log('Invalid status saving audio message: ' + res.status);
+        for (; i < length; i++) {
+            values += array[i];
+        }
+
+        return values / length;
+    }; // Render the bars
+
+
+    const renderBars = bars => {
+        if (!drawing) {
+        drawing = true;
+        window.requestAnimationFrame(() => {
+            canvasContext.clearRect(0, 0, width, height);
+            bars.forEach((bar, index) => {
+            canvasContext.fillStyle = barColor;
+            canvasContext.fillRect(index * (barWidth + barGutter), halfHeight, barWidth, halfHeight * (bar / 100));
+            canvasContext.fillRect(index * (barWidth + barGutter), halfHeight - halfHeight * (bar / 100), barWidth, halfHeight * (bar / 100));
             });
-        };
-    });
-
-    const populateAudioMessages = () => {
-        return fetch('/messages').then(res => {
-            if (res.status === 200) {
-                return res.json().then(json => {
-                    json.messageFilenames.forEach(filename => {
-                        let audioElement = document.querySelector(`[data-audio-filename="${filename}"]`);
-                        if (!audioElement) {
-                            audioElement = document.createElement('audio');
-                            audioElement.src = `/messages/${filename}`;
-                            audioElement.setAttribute('data-audio-filename', filename);
-                            audioElement.setAttribute('controls', true);
-                            savedAudioMessagesContainer.appendChild(audioElement);
-                        }
-                    });
-                });
-            }
-            console.log('Invalid status getting messages: ' + res.status);
+            drawing = false;
         });
-    };
+        }
+    }; // Play the recording
 
-    populateAudioMessages();
+
+    const play = () => {
+        isPlaying = true;
+        audioPlayer.play();
+        playButton.classList.add('button--active');
+        playButtonIcon.classList.add('bi-pause-fill');
+        playButtonIcon.classList.remove('bi-play-fill');
+    }; // Stop the recording
+
+
+    const stop = () => {
+        isPlaying = false;
+        audioPlayer.pause();
+        audioPlayer.currentTime = 0;
+        playButton.classList.remove('button--active');
+        playButtonIcon.classList.add('bi-play-fill');
+        playButtonIcon.classList.remove('bi-pause-fill');
+    }; // Toggle the play button
+
+
+    const togglePlay = () => {
+        if (isPlaying) {
+            stop();
+        } else {
+            play();
+        }
+    }; // Setup the audio player
+
+
+    const setupPlayer = () => {
+        audioPlayer.addEventListener('ended', () => {
+            stop();
+        });
+    }; // Start the application
+
+
+    requestMicrophoneAccess();
+    setupPlayer(); // Add event listeners to the buttons
+
+    //recordButton.addEventListener('mouseup', toggleRecording);
+
+    function computeElapsedTime(startTime) {
+        //record end time
+        let endTime = new Date();
+    
+        //time difference in ms
+        let timeDiff = endTime - startTime;
+    
+        //convert time difference from ms to seconds
+        timeDiff = timeDiff / 1000;
+    
+        //extract integer seconds that dont form a minute using %
+        let seconds = Math.floor(timeDiff % 60); //ignoring uncomplete seconds (floor)
+    
+        //pad seconds with a zero if neccessary
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+        //convert time difference from seconds to minutes using %
+        timeDiff = Math.floor(timeDiff / 60);
+    
+        //extract integer minutes that don't form an hour using %
+        let minutes = timeDiff % 60; //no need to floor possible incomplete minutes, becase they've been handled as seconds
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+    
+        //convert time difference from minutes to hours
+        timeDiff = Math.floor(timeDiff / 60);
+    
+        //extract integer hours that don't form a day using %
+        let hours = timeDiff % 24; //no need to floor possible incomplete hours, becase they've been handled as seconds
+    
+        //convert time difference from hours to days
+        timeDiff = Math.floor(timeDiff / 24);
+    
+        // the rest of timeDiff is number of days
+        let days = timeDiff; //add days to hours
+    
+        let totalHours = hours + (days * 24);
+        totalHours = totalHours < 10 ? "0" + totalHours : totalHours;
+    
+        if (totalHours === "00") {
+            return minutes + ":" + seconds;
+        } else {
+            return totalHours + ":" + minutes + ":" + seconds;
+        }
+    }
+
+    recordButton.onclick = function(e) { 
+        e.preventDefault();
+        startRecording();
+        recordButton.classList.add('button--active');
+        recordButton.style.display = "none";
+        stopButton.style.display = 'block';
+        recorderBox.classList.add('recorder');
+        setMessage('Recording...');
+        //Handle the displaying of the elapsed recording time
+        elapsedTimeTimer = setInterval(() => {
+            //compute the elapsed time every second
+            let elapsedTime = computeElapsedTime(audioRecordStartTime); //pass the actual record start time
+            //display the elapsed time
+            elapsedTimeTag.innerHTML = elapsedTime;
+        }, 1000); //every second
+    }
+
+    stopButton.onclick = function(e) { 
+        e.preventDefault();
+        stopRecording();
+        recordButton.classList.remove('button--active');
+        stopButton.style.display = "none";
+        playButton.style.display = "block";
+        setMessage('');
+        var _btnshowcomment = $("<a/>")
+            .addClass("btn btn-remove-record")
+            .attr("href", "#")
+            .attr("role", "button")
+            .attr("data-toggle", "modal")
+            .html('<i class="bi bi-trash-fill"></i>')
+            .on("click", function (e) {
+                e.preventDefault();
+                $('#removeVoiceNoteModal').modal('toggle');
+            });
+        $('.js-message').html(_btnshowcomment);
+        //stop interval that handles both time elapsed and the red dot
+        clearInterval(elapsedTimeTimer);
+    }
+
+    removeRecorder.onclick = function (e) {
+        e.preventDefault();
+        if (isPlaying) {
+            stop();
+        }
+        $('.btn-remove-record').css('display','none');
+        recordButton.style.display = "block";
+        playButton.style.display = "none";
+        recorderBox.classList.remove('recorder');
+        $('#removeVoiceNoteModal').modal('toggle');
+
+    }
+    
+    // playButton.addEventListener('mouseup', togglePlay);
+    playButton.onclick = function (e) {
+        e.preventDefault();
+        togglePlay();
+    }
 })();
